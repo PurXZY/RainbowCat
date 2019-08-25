212439ee9efd859bab4c3ea0a436cbf2d4ed6410
Pur <844840584@qq.com>
2019-08-25 13:53:45 +0800

    import pig

---
+++ Assets/Scripts/PigController.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PigController : MonoBehaviour
+{
+    public List<Vector2> patrolPos;
+    int patrolIndex = 0;
+    bool isInPatrol = true;
+    float speed = 2.0f;
+    private Animator animator;
+    public Vector2 curDir
+    {
+        private set;
+        get;
+    }
+
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    void FixedUpdate()
+    {
+        if (isInPatrol)
+            patrol();
+    }
+
+    void patrol()
+    {
+        var targetPos = patrolPos[patrolIndex];
+        var dirV = targetPos - (Vector2)transform.position;
+        curDir = dirV.normalized;
+        animator.SetFloat("speedX", curDir.x);
+        animator.SetFloat("speedY", curDir.y);
+        transform.Translate(curDir * speed * Time.deltaTime);
+
+        if (dirV.magnitude < 0.1f)
+        {
+            changePatrolIndex();
+        }
+    }
+
+    void changePatrolIndex()
+    {
+        patrolIndex = patrolIndex + 1 >= patrolPos.Count ? 0 : patrolIndex + 1;
+    }
+}
---
+++ Assets/Scripts/SightController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+
+using System.Collections.Generic;
+
+using UnityEngine;
+
+public class SightController : MonoBehaviour
+{
+    void Update()
+    {
+        DrawTriangle(transform, transform.position, 60, 3, GetComponent<PigController>().curDir);
+    }
+
+    private static LineRenderer GetLineRenderer(Transform t)
+    {
+        LineRenderer lr = t.GetComponent<LineRenderer>();
+        if (lr == null)
+        {
+            lr = t.gameObject.AddComponent<LineRenderer>();
+        }
+        lr.startWidth = 0.05f;
+        lr.endWidth = 0.05f;
+        return lr;
+    }
+
+    public static void DrawTriangle(Transform t, Vector3 startPos, float angle, float radius, Vector2 curDir)
+    {
+        LineRenderer lr = GetLineRenderer(t);
+        int pointAmount = 50;
+        float eachAngle = angle / pointAmount;
+
+        lr.positionCount = pointAmount;
+        startPos = startPos + new Vector3(0, 0, -1);
+        lr.SetPosition(0, startPos);
+        for (int i = 1; i < pointAmount-1; ++i)
+        {
+            Vector3 pos = startPos + Quaternion.Euler(0f, 0f, -angle/2 + eachAngle * (i - 1)) * curDir * radius;
+            lr.SetPosition(i, pos);
+        }
+        lr.SetPosition(pointAmount-1, startPos);
+    }
+}
