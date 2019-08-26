bc73b04910b07429b8f3c7aa3eb4553086bee0a4
Pur <844840584@qq.com>
2019-08-26 11:15:23 +0800

    add chest show bubble

---
+++ Assets/Scripts/ChestController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChestController : MonoBehaviour
+{
+    public GameObject bubble;
+
+    private void OnTriggerEnter2D(Collider2D coll)
+    {
+        if (coll.gameObject.CompareTag("Player"))
+        {
+            ShowBubble();
+        }
+    }
+    
+    private void ShowBubble()
+    {
+        bubble.SetActive(true);
+        // bubble.GetComponent<Animation>().Play("bubble_idle");
+    }
+}
